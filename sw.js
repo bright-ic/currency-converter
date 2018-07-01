f5e8c7f1b1ddaebe25811004bec8c13527c05196
bright-ic <40720995+bright-ic@users.noreply.github.com>
2018-07-01 22:42:57 +0100

    Update sw.js
    Made changes to javascript use >= ES6

--- sw.js
+++ sw.js
@@ -2,37 +2,36 @@ const staticCacheName = 'BrightsCurrencyConverter-static-v4';
 
 // files to cache
 const filesToCache = [
-  './',
-  './index.html',
-  './scripts/appcontroller.js',
-  './css/styles.css',
-  './manifest.json',
+  '/',
+  'index.html',
+  'scripts/appcontroller.js',
+  'css/styles.css',
+  'manifest.json',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
     console.log('Installing service worker.');
     event.waitUntil(
-      caches.open(staticCacheName).then(function(cache) {
+      caches.open(staticCacheName).then( cache => {
         console.log('service worker installed successfully.');
         return cache.addAll(filesToCache);
-      }).catch( error => console.log('failed to cache: ' + error))
+      })
     );
   });
   //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  self.addEventListener('activate', function(event) {
+  self.addEventListener('activate', event => {
     console.log('service worker activated successfully');
     event.waitUntil(
-      caches.keys().then(function(cacheNames) {
+      caches.keys().then( cacheNames => {
         return Promise.all(
-          cacheNames.filter(function(cacheName) {
+          cacheNames.filter(cacheName => {
              // console.log(cacheName);
             return cacheName.startsWith('BrightsCurrencyConverter-') && staticCacheName !== cacheName;
-          }).map(function(cacheName) {
+          }).map( cacheName => {
             if(staticCacheName !== cacheName){
                 return caches.delete(cacheName);
-                //console.log(cacheName);
             }
           })
         );
@@ -49,7 +48,7 @@ self.addEventListener('install', function(event) {
         caches.match(event.request).then(response => {
           if (response) {
             // respond with the index page skeleton in cache
-             event.respondWith(caches.match('/ndex.html'));
+             event.respondWith(caches.match('/index.html'));
              return;
           }
         });
@@ -65,4 +64,4 @@ self.addEventListener('install', function(event) {
       })
     );
 
-  });
+});
